a55a30f6fcbb73634cc987079e1663a974871c35
NoahSchiro <noahschiro@rocketmail.com>
2021-04-01 19:05:12 -0400

    Update binaryTree.cpp

--- C++ Trees/C++ Binary Tree/binaryTree.cpp
+++ C++ Trees/C++ Binary Tree/binaryTree.cpp
@@ -29,6 +29,48 @@ class binaryTree {
         binaryTree() {
             root = nullptr;
         }
+
+        //Inserts a new node into tree
+        void insert(int information) {
+
+            BTnode* temp = root, *previous = nullptr;
+
+            //While our temp variable has not 
+            //reached a leaf node, continue (skips this 
+            //while loop if the tree is intially empty)
+            while(temp != nullptr) {
+
+                //Let the previous node equal
+                //the node we are focusing on  
+                previous = temp;
+                
+                //If our input variable is less then
+                //the node we are focusing on, then
+                //move down to the left side of the tree 
+                if(information < temp -> data) {
+                    
+                    temp = temp -> left;
+                
+                //Same as above but greater then -> right
+                } else {
+
+                    temp = temp -> right;
+                }
+            }
+
+            //Executes if tree is empty
+            if(root == nullptr) {
+            
+                root = new BTnode(information);
+
+            //Creates a new leaf
+            } else if (information < previous -> data) {
+                previous -> left = new BTnode(information);
+            } else {
+                previous -> right = new BTnode(information);
+            }
+
+        }
 };
 
 #endif
